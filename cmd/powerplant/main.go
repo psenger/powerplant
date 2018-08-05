846fc00497519fde886fc8e3f32be6e931efc91e
Philip A Senger <philip.a.senger@cngrgroup.com>
2018-08-05 22:10:35 +1000

    init

--- cmd/powerplant/main.go
+++ cmd/powerplant/main.go
@@ -3,8 +3,69 @@ package main
 import (
 	"fmt"
 	"errors"
-	"github.com/psenger/powerplant"
+	"strings"
 )
+
+type PlantType string
+
+const (
+	hydro PlantType = "hydro"
+	wind PlantType = "wind"
+	solar PlantType = "solar"
+)
+
+type PlantStatus string
+
+const (
+	active PlantStatus = "active"
+	inactive PlantStatus = "inactive"
+	unavailable PlantStatus = "unavailable"
+)
+
+type PowerPlant struct {
+	plantType PlantType
+	capacity float64
+	status PlantStatus
+
+}
+
+type PowerGrid struct {
+	load float64
+	plants []PowerPlant
+}
+
+func (pg *PowerGrid) generatePlantReport() {
+	for idx, p := range pg.plants {
+
+		label := fmt.Sprintf("%s%d", "Plant #", idx )
+		fmt.Println(label)
+		fmt.Println(strings.Repeat("-", len(label)))
+
+		fmt.Printf("%-20s%s\n", "Type:", p.plantType)
+		fmt.Printf("%-20s%.0f\n", "Capacity:", p.capacity)
+		fmt.Printf("%-20s%s\n", "Status:", p.status)
+
+		fmt.Printf("")
+	}
+}
+
+func (pg *PowerGrid) generateGridReport() {
+	capacity := 0.
+	for _, p := range pg.plants {
+		if p.status == active {
+			capacity += p.capacity
+		}
+	}
+
+	label := "Power Grid Report"
+	fmt.Println(label)
+	fmt.Println(strings.Repeat("-", len(label)))
+
+	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load:", pg.load)
+	fmt.Printf("%-20s%.2f%%\n", "Utilization:", pg.load/capacity*100)
+
+}
 
 func main() {
 
